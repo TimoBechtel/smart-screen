812ff63ec09c1acc1fb69260b30047a7b90d8fbf
tbechtel <timo.bechtel@smail.th-koeln.de>
2021-06-17 00:55:58 +0200

    feat: random 404

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,19 @@
-<script lang="ts">
-	let id;
-</script>
+<main>
+	{#each new Array(1000).fill(404).map((i) => (Math.random() < 0.01 ? 420 : i)) as number}
+		<p>
+			{number}
+		</p>
+	{/each}
+</main>
 
-<form
-	action="#"
-	on:submit={(e) => {
-		e.preventDefault();
-		location.pathname = `/${id}/dashboard`;
-	}}
->
-	<input type="text" placeholder="id" bind:value={id} />
-	<button>open id</button>
-</form>
+<style lang="scss">
+	main {
+		display: flex;
+		max-width: 100vw;
+		flex-wrap: wrap;
+		opacity: 0.3;
+		p {
+			margin: 5px;
+		}
+	}
+</style>
